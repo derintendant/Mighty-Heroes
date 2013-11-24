cf91004c4a5010d82609e6d206f991c8b04954ed
Stollebob <Thosto92@web.de>
2013-11-24 16:29:12 +0100

    Created a class to comunicate with the Server. Added the XmLRpc client. The LoginController is now able to send a LoginRequest to the Server.

---
+++ MIGLogin/MIGXmlRpcClient/src/MIG/ka/bprocess/Networking/NetworkingClient.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package MIG.ka.bprocess.Networking;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.xmlrpc.XmlRpcException;
+import org.apache.xmlrpc.client.XmlRpcClient;
+import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
+
+/**
+ *
+ * @author Thor
+ */
+public class NetworkingClient {
+    
+    private static NetworkingClient instance = null;
+    
+    private final static String ServerUrlPrefix = "http://";
+    private final static String ServerIp = "127.0.0.1";
+    private final static String ServerPort = ":8080/";
+    
+    private final XmlRpcClient client;
+
+    private NetworkingClient() {
+        this.client = new XmlRpcClient();
+        XmlRpcClientConfigImpl conf = new XmlRpcClientConfigImpl();
+        try 
+        {
+            conf.setServerURL(new URL(ServerUrlPrefix+ServerIp+ServerPort));
+        } 
+        catch (MalformedURLException ex) 
+        {
+            Logger.getLogger(NetworkingClient.class.getName()).log(Level.SEVERE, "Server URL is not working!" , ex);
+        }
+        this.client.setConfig(conf);
+    }
+    
+    private Object sendWithParams(String MethodName, ArrayList params)
+    {
+        try 
+        {
+           return this.client.execute(MethodName, params);
+        } 
+        catch (XmlRpcException ex) 
+        {
+            Logger.getLogger(NetworkingClient.class.getName())
+                    .log(Level.SEVERE, "Failed Serverrequest for: " + MethodName + "with following Parameters: " + params, ex);
+        }
+        return null;
+    }
+    
+    public boolean LoginRequest(String username, String password)
+    {
+        ArrayList list = new ArrayList();
+        list.add(username);
+        list.add(password);
+        return (boolean) sendWithParams("LoginRequest", list);
+    }
+    
+    public static NetworkingClient getInstance()
+    {
+        if(instance == null)
+        {
+           instance = new NetworkingClient();
+        }
+        return instance;
+    }
+}
